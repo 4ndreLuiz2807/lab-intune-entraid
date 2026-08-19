a1e7461388e442d51dad497f627f26448c3c121f
4ndreLuiz2807 <andrelufe201034@gmail.com>
2026-08-19 20:27:57 -0300

    Add files via upload

---
+++ APLICAÇÕES/SAP/Detect SAP.ps1
@@ -0,0 +1,17 @@
+﻿$SAP = Get-ItemProperty `
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
+    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" `
+    -ErrorAction SilentlyContinue |
+    Where-Object {
+        $_.DisplayName -like "SAP GUI for Windows 8.00*"
+    } |
+    Select-Object -First 1
+
+if ($SAP) {
+
+    Write-Output "SAP GUI detectado: $($SAP.DisplayName)"
+    exit 0
+
+}
+
+exit 1
